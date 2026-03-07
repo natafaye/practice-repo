de1d57c3c50f7b20ba111b5cae1064300227e878
juan <juanpabloblo14@gmail.com>
2026-03-06 17:34:18 -0800

    js

--- juan.js
+++ juan.js
@@ -5,3 +5,14 @@ let isthisjuan = true
 
 alert("hi "+favoriteColor)
 console.log("this is juan " + isthisjuan + " " + favoritenumber)
+
+//speed fast
+//price not expensive 
+//color red
+
+let priceExpencive = false
+let speed = true
+let color = true
+
+let shouldbuy = speed && !priceExpencive && color
+console.log("should i buy the car " + shouldbuy)
