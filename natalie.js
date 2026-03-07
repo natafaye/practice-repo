3793dd758da17a1be5448a0e3aae6a373443a39b
Natalie Childs <nata.faye@gmail.com>
2026-03-06 16:21:05 -0800

    created javascript variables and message

---
+++ natalie.js
@@ -0,0 +1,12 @@
+// Goal:
+// 1) Make a JS file and connect it to your HTML page
+// 2) Make 3 variables (a string, a number, a boolean)
+// 3) Use the 3 variables to show 2 messages (alert and console)
+
+
+let favoriteColor = "green" // variable with a string
+let isTeacher = true // variable with a boolean
+let score = 1000 // variable with a number
+
+alert("Hello " + favoriteColor + "! Your score is: " + score)
+console.log("Are you a teacher? " + isTeacher)
