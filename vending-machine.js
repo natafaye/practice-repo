cdfa549b6fd3be6d62a2ef02b4ce9942774c0374
Natalie Childs <nata.faye@gmail.com>
2026-03-13 17:02:32 -0700

    vending machine start

---
+++ vending-machine.js
@@ -0,0 +1,6 @@
+// Every time you click the money button, you're adding $1 to the machine
+// Keep track of that total
+
+// If it's already been bought, you get an error message
+// If you don't have enough money, you get an error message
+// If neither of those issues, you buy it (and your money goes to zero)
